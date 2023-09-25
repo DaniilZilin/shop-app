bef122796a7319db25471c1ef3fcbb0b06e39070
unknown <danilasilin@yandex.ru>
2023-09-26 03:24:10 +0400

    removed comms

--- app/views/Order/index.tsx
+++ app/views/Order/index.tsx
@@ -3,14 +3,12 @@ import React from 'react'
 import dynamic from 'next/dynamic';
 import CreditCard from '../../components/modules/order/creditCardForm'
 import UserForm from '../../components/modules/order/userForm'
-// import Maps from '../../components/modules/order/addressInfo'
 
 const Maps = dynamic(() => import('../../components/modules/order/addressInfo'), {
   ssr: false,
 });
 
 export interface Props {
-
 }
 
 
